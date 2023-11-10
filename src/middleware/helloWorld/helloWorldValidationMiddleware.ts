9198ddb236136b4e159bd81abe96daa7db5c67b2
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-11-10 11:58:54 +0000

    moves helloWorld validation middleware

--- src/middleware/helloWorld/helloWorldValidationMiddleware.ts
+++ src/middleware/helloWorld/helloWorldValidationMiddleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
-import { isHelloWorldRequestBody } from '../utils/types/helloWorld'
+import { isHelloWorldRequestBody } from '../../utils/types/helloWorld'
 
 export const helloWorldValidationMiddleware = (
   request: Request,
