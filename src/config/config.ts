90e3daacbe95cf71db959f0a02185241fb915214
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-11-17 11:04:44 +0000

    adds extra views to APP_PATH to allow use of internalServerError tempalte

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,7 @@
 import path from 'path'
 
 export const PORT = 3000
-export const APP_VIEWS = [path.resolve(__dirname, '..', 'components')]
+export const APP_VIEWS = [
+  path.resolve(__dirname, '..', 'components'),
+  path.resolve(__dirname, '..', 'views')
+]
