33fdfa7f2812c5bb3bd5c8bcffd3bfc0877ecdd7
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-11-17 11:03:26 +0000

    adds testEnvironmentVars and setup to unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import { JestConfigWithTsJest } from 'ts-jest'
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/*'],
-  testMatch: ['<rootDir>/src/**/*.test.ts']
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  setupFiles: ['<rootDir>src/utils/test/testEnvironmentVariables.ts']
 }
 
 export default config
