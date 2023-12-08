1c4fa0c28a100df94a4d5be7f797cb830683a996
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-12-08 14:10:33 +0000

    adds hashValue helper function

--- src/utils/doesHashMatch.ts
+++ src/utils/doesHashMatch.ts
@@ -1,13 +1,11 @@
-import { createHmac } from 'crypto'
 import { getEnv } from './getEnv'
+import { hashValue } from './hashValue'
 
 export const doesHashMatch = (
   value: string,
   salt: string,
   hashedValue: string
 ): boolean => {
-  const valueToCheck = createHmac('sha256', getEnv('HASH_SECRET'))
-    .update(value + salt)
-    .digest('hex')
+  const valueToCheck = hashValue(value, salt, getEnv('HASH_SECRET'))
   return valueToCheck === hashedValue
 }
